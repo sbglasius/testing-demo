f48397cc4ad793a30b31ae3dbb7da90e20f33770
Søren Berg Glasius <sbg@nine.dk>
2022-09-22 18:16:18 +0200

    Mock example

--- src/test/groovy/testing/demo/AdresseControllerSpec.groovy
+++ src/test/groovy/testing/demo/AdresseControllerSpec.groovy
@@ -1,18 +1,27 @@
 package testing.demo
 
+import client.AdresseClient
 import grails.testing.web.controllers.ControllerUnitTest
+import model.AdresseInfo
 import spock.lang.Specification
 
 class AdresseControllerSpec extends Specification implements ControllerUnitTest<AdresseController> {
 
-    def setup() {
+    void setup() {
+        controller.adresseClient = Mock(AdresseClient)
     }
 
-    def cleanup() {
+    void "find adresser"() {
+        given:
+        params.adresse = 'Et sted'
+        when:
+        controller.find()
+
+        then:
+        1 * controller.adresseClient.findAllAdresser('Et sted') >> [new AdresseInfo(tekst: 'kryf')]
+
+        and:
+        response.json.size() == 1
     }
 
-    void "test something"() {
-        expect: "fix me"
-        true == false
-    }
 }
